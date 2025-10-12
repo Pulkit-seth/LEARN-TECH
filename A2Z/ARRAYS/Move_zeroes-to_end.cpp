caff7de82067b833a9fb8acab8d57cdd5c449886
Pulkit seth <2432427pulkit@gmail.com>
2025-10-12 23:27:15 +0530

    Add files via upload

---
+++ A2Z/ARRAYS/Move_zeroes-to_end.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void moveZeroes(vector<int>& v)
+{
+    int j = -1;
+    int n = v.size();
+
+    for (int i = 0; i < n; i++)
+    {
+        if (v[i] == 0)
+        {
+            j = i;
+            break;
+        }
+    }
+    for (int i = j + 1; i < n; i++)
+    {
+        if (v[i] != 0)
+        {
+            swap(v[i], v[j]);
+            j++;
+        }
+    }
+}
+int main()
+{
+    vector<int> v = {1, 0, 3, 0, 9, 0, 7, 0, 9};
+    cout << "Before Rotate" << endl;
+    for (auto it : v)
+    {
+        cout << it << " ";
+    }
+    cout << endl;
+    moveZeroes(v);
+    cout << "After Rotate" << endl;
+    for (auto it : v)
+    {
+        cout << it << " ";
+    }
+    return 0;
+}
