4ffc0a59b45a5bd77778f09181662f46f04431e3
Pulkit seth <2432427pulkit@gmail.com>
2025-11-26 18:49:34 +0530

    Add files via upload

---
+++ A2Z/QUEUE/Circular_Queue_dynamic.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+using namespace std;
+struct Node
+{
+    int data;
+    Node *next;
+};
+Node *front = NULL;
+Node *rear = NULL;
+
+void enqueue(int x)
+{
+    Node *newnode = new Node();
+    if (!newnode)
+    {
+        cout << "Queue Overflow" << endl;
+        return;
+    }
+    newnode->data = x;
+    newnode->next = NULL;
+    if (front == NULL)
+    {
+        front = rear = newnode;
+        return;
+    }
+    else
+    {
+        rear->next = newnode;
+        rear = newnode;
+    }
+}
+
+void dequeue()
+{
+    Node *temp;
+    if (front == NULL)
+    {
+        cout << "Queue Underflow" << endl;
+        return;
+    }
+    temp = front;
+    cout << "Dequeued element is: " << temp->data << endl;
+    front = front->next;
+    if (front == NULL)
+    {
+        rear = NULL;
+    }
+   delete (temp);
+    temp = NULL;
+}
+
+void peek()
+{
+    if (front == NULL)
+    {
+        cout << "Queue is empty" << endl;
+        return;
+    }
+    cout << "Front element is: " << front->data << endl;
+}
+
+void display()
+{
+    Node *temp;
+    if (front == NULL)
+    {
+        cout << "Queue is empty" << endl;
+        return;
+    }
+    temp = front;
+    cout << "Queue elements are: ";
+    while (temp != NULL)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+int main()
+{
+    enqueue(10);
+    enqueue(20);
+    enqueue(30);
+    display();
+    peek();
+    dequeue();
+    display();
+    peek();
+    return 0;
+}
