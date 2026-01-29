19073b06bcc3ce9df6637c53a696076ff464d8f8
Pulkit seth <2432427pulkit@gmail.com>
2026-01-29 22:48:36 +0530

    Remove and re-add SQL statements for Employee table

--- SQL/first.sql
+++ SQL/first.sql
@@ -1,8 +1,8 @@
 create table Employee (EmpId number(5), EmpName varchar(20), Dept varchar(10), Sal numeric(6,2));
 insert into Employee values(10001, 'Pulkit', 'HR', 9000.00);
 insert into Employee values(10002, 'Avi', 'Manager', 900.00);
-
 Select *from Employee;
 Select EmpId, Empname from Employee;
+
 
 
