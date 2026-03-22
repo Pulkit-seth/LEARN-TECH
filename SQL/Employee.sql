50063721ae7f99bb9c9414e789ef759c546703ad
Pulkit seth <2432427pulkit@gmail.com>
2026-03-22 21:08:58 +0530

    Add new employee record to Employee table

--- SQL/Employee.sql
+++ SQL/Employee.sql
@@ -1,6 +1,7 @@
 create table Employee (EmpId number(5), EmpName varchar(20), Dept varchar(10), Sal numeric(6,2));
 insert into Employee values(10001, 'Pulkit', 'HR', 9000.00);
 insert into Employee values(10002, 'Avi', 'Manager', 900.00);
+insert into Employee values(10003, 'Aviral', 'Manager', 950.00);
 Select *from Employee;
 Select EmpId, Empname from Employee;
 
