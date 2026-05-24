92eb590be3f94c743d8b4fd3a4890b07417feb7b
Pulkit seth <2432427pulkit@gmail.com>
2026-05-24 21:02:12 +0530

    Refactor variable names for clarity in Koko's problem

--- A2Z/ARRAYS/Koko_Eating_Bananas.cpp
+++ A2Z/ARRAYS/Koko_Eating_Bananas.cpp
@@ -3,20 +3,20 @@ int kokoEat(vector<int>& arr, int k) {
         int n = arr.size();
         sort(arr.begin(), arr.end());
         
-        int lo = 1, hi = arr[n-1];
+        int low = 1, high = arr[n-1];
         
-        int min_s = lo + (hi-lo)/2 ;
+        int min_s = low + (high-low)/2 ;
 
-        while (lo<hi) {
+        while (low<high) {
             int tem = 0;
-            min_s = lo + (hi-lo)/2 ;
+            min_s = low + (high-low)/2 ;
             // cout << lo << ' ' << min_s << ' ' << hi << '\n';
             for (int x:arr) {
                 tem += (x+min_s-1)/min_s;
             }
             if (tem<=k) hi = min_s;
-            else lo = min_s+1;
+            else low = min_s+1;
         }
         
-        return lo;
+        return low;
     }
